82fa9748f0ae3a2edec8ae48281d35c19d00a5a6
Jake Hobbs <jakehobbs@gmail.com>
2020-11-13 16:14:36 -0800

    add endpoints for managing roles via local api

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const express = require('express')
+const app = express()
+const port = 6070
 
 const WELCOME_NEW_MEMBERS = true
 
@@ -21,21 +24,21 @@ client.on('message', msg => {
   // reply if message sent directly to you
   if (msg.channel.type === "dm") {
 
-    // get the user's ID
-    userDiscordID = msg.author.id
-    console.log(userDiscordID);
+	  // get the user's ID
+	  userDiscordID = msg.author.id
+	  console.log(userDiscordID);
 
-    // get status of user id in ADB
-    const paramsForStatus = new URLSearchParams();
-	paramsForStatus.append('auth', process.env.ADB_SECRET);
-	paramsForStatus.append('id', userDiscordID);
+	  // get status of user id in ADB
+	  const paramsForStatus = new URLSearchParams();
+		paramsForStatus.append('auth', process.env.ADB_SECRET);
+		paramsForStatus.append('id', userDiscordID);
 
-    fetch('https://adb.dxe.io/discord/status', {method: 'POST', body: paramsForStatus})
-	.then( res => { return res.json() } )
-	.then( json => {
-		status = json.status
+  	fetch('https://adb.dxe.io/discord/status', {method: 'POST', body: paramsForStatus})
+		.then( res => { return res.json() } )
+		.then( json => {
+			status = json.status
 
-		// TODO: if user status is confirmed, just say g'day & that you regret to inform them that there is nothing else you can do for them at this time
+			// TODO: if user status is confirmed, just say g'day & that you regret to inform them that there is nothing else you can do for them at this time
 	    if (status == 'confirmed') {
 	    	msg.reply("Your identify has been confirmed. There's nothing else that I can do for you at this time. Have a good day!")
 	    	return
@@ -57,28 +60,28 @@ client.on('message', msg => {
 
 	    // make "generate" request to ADB - it will return "invalid email" if not found or "success" if email belongs to an activist
 	    const paramsForGenerate = new URLSearchParams();
-		paramsForGenerate.append('auth', process.env.ADB_SECRET);
-		paramsForGenerate.append('id', userDiscordID);
-		paramsForGenerate.append('email', userEmail);
+			paramsForGenerate.append('auth', process.env.ADB_SECRET);
+			paramsForGenerate.append('id', userDiscordID);
+			paramsForGenerate.append('email', userEmail);
 
-		fetch('https://adb.dxe.io/discord/generate', {method: 'POST', body: paramsForGenerate})
-		.then( res => { return res.json() } )
-		.then( json => {
-			status = json.status
+			fetch('https://adb.dxe.io/discord/generate', {method: 'POST', body: paramsForGenerate})
+			.then( res => { return res.json() } )
+			.then( json => {
+				status = json.status
 
-			if (status === 'invalid email') {
-				msg.reply("Sorry, I could not find any activists associated with that email address. Please enter another email address to check, or email tech@dxe.io for assistance.")
-				return
-			}
+				if (status === 'invalid email') {
+					msg.reply("Sorry, I could not find any activists associated with that email address. Please enter another email address to check, or email tech@dxe.io for assistance.")
+					return
+				}
 
-			if (status === 'success') {
-				msg.reply("I just sent an email to you to confirm your email address. Please click the confirmation link in the email. If you can't find it, then be sure to check your spam folder.")
-				return
-			}
+				if (status === 'success') {
+					msg.reply("I just sent an email to you to confirm your email address. Please click the confirmation link in the email. If you can't find it, then be sure to check your spam folder.")
+					return
+				}
+
+			})
 
 		})
-
-	})
 
   }
 
@@ -100,3 +103,135 @@ if (WELCOME_NEW_MEMBERS) {
 }
 
 client.login(process.env.DISCORD_TOKEN);
+
+app.get('/roles/get', (req, res) => {
+
+	// TODO: check that request is authorized with our ADB/bot shared secret
+
+	// fetch the guild's roles so that we know the names
+	client.guilds.fetch(process.env.DISCORD_GUILD_ID)
+	.then(guild => {
+		guild.roles.fetch()
+		.then(roles => {
+			let allRoles = new Map()
+			roles.cache.forEach(role => {
+				allRoles[role.id] = role.name
+			})
+
+			// fetch the user's roles
+			client.guilds.fetch(process.env.DISCORD_GUILD_ID)
+			.then(guild => {
+				guild.members.fetch(req.query.user)
+				.then(user => {
+					roles = user._roles
+					// get names of roles that the user has
+					let userRoles = new Map()
+					roles.forEach(role => {
+						userRoles[role] = allRoles[role]
+					})
+					res.json(userRoles);
+				})
+			})
+
+		})
+	})
+
+})
+
+app.post('/roles/add', (req, res) => {
+	let role = req.query.role
+
+	// if role is not a number, see if you can find a role with that name
+	if (isNaN(role)) {
+		// try look up the id using the name
+		client.guilds.fetch(process.env.DISCORD_GUILD_ID)
+		.then(guild => {
+			guild.roles.fetch()
+			.then(roles => {
+				let allRoles = new Map()
+				roles.cache.forEach(role => {
+					allRoles[role.name] = role.id
+				})
+				role = allRoles[role]
+				if (role === undefined) return res.json({"result": "role not found"});
+				
+				// try to add the role using the ID - TODO: move this to its own function to be reusable (or just use async/await to control flow better)
+				console.log("Adding " + role + " role to user " + req.query.user)
+				client.guilds.fetch(process.env.DISCORD_GUILD_ID)
+				.then(guild => {
+					guild.members.fetch(req.query.user)
+					.then(user => {
+						user.roles.add(role)
+						res.json({"result": "added"});
+					})
+				})
+
+			})
+		})
+	} else {
+
+		// try to add the role using the ID - TODO: move this to its own function to be reusable (or just use async/await to control flow better)
+		console.log("Adding " + role + " role to user " + req.query.user)
+		client.guilds.fetch(process.env.DISCORD_GUILD_ID)
+		.then(guild => {
+			guild.members.fetch(req.query.user)
+			.then(user => {
+				user.roles.add(role)
+				res.json({"result": "added"});
+			})
+		})
+
+	}
+
+})
+
+app.post('/roles/remove', (req, res) => {
+	let role = req.query.role
+
+	// if role is not a number, see if you can find a role with that name
+	if (isNaN(role)) {
+		// try look up the id using the name
+		client.guilds.fetch(process.env.DISCORD_GUILD_ID)
+		.then(guild => {
+			guild.roles.fetch()
+			.then(roles => {
+				let allRoles = new Map()
+				roles.cache.forEach(role => {
+					allRoles[role.name] = role.id
+				})
+				role = allRoles[role]
+				if (role === undefined) return res.json({"result": "role not found"});
+				
+				// try to add the role using the ID - TODO: move this to its own function to be reusable (or just use async/await to control flow better)
+				console.log("Removing " + role + " role from user " + req.query.user)
+				client.guilds.fetch(process.env.DISCORD_GUILD_ID)
+				.then(guild => {
+					guild.members.fetch(req.query.user)
+					.then(user => {
+						user.roles.remove(role)
+						res.json({"result": "removed"});
+					})
+				})
+
+			})
+		})
+	} else {
+
+		// try to add the role using the ID - TODO: move this to its own function to be reusable (or just use async/await to control flow better)
+		console.log("Removing " + role + " role from user " + req.query.user)
+		client.guilds.fetch(process.env.DISCORD_GUILD_ID)
+		.then(guild => {
+			guild.members.fetch(req.query.user)
+			.then(user => {
+				user.roles.remove(role)
+				res.json({"result": "removed"});
+			})
+		})
+
+	}
+
+})
+
+app.listen(port, () => {
+	console.log(`App listening at http://localhost:${port}`)
+})
