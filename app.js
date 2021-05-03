266f9332f50b357bde034264ab55e3fdf9a75f2c
Jake Hobbs <jakehobbs@gmail.com>
2021-05-03 15:56:56 -0700

    log auth header

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const state = {
 const msgCharLimit = 1900
 
 const authorizePostRequests = function (req, res, next) {
+	console.log("Auth header: " + req.header["Auth"])
 	if (req.method === "POST" && req.header["Auth"] !== ADB_SECRET) {
 		res.status(400);
 		return res.json({"result": "not authorized"});
