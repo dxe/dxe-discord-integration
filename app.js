d780d8f37921f67560b430590ad0c5f89b8b43f1
Jake Hobbs <jakehobbs@gmail.com>
2021-05-03 16:05:12 -0700

    bug fix

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ const state = {
 const msgCharLimit = 1900
 
 const authorizePostRequests = function (req, res, next) {
-	console.log("Auth header: " + req.headers["auth"])
-	if (req.method === "POST" && req.headers["Auth"] !== ADB_SECRET) {
+	if (req.method === "POST" && req.headers["auth"] !== ADB_SECRET) {
 		res.status(400);
 		return res.json({"result": "not authorized"});
 	}
